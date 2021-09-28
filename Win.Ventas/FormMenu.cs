116c1cca636ad3b3758011900acfe85da6c6ea73
TaniaVarela <91446212+TaniaVarela@users.noreply.github.com>
2021-09-27 19:30:14 -0600

    Add files via upload

---
+++ Win.Ventas/FormMenu.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.Ventas
+{
+    public partial class FormMenu : Form
+    {
+        public FormMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void clientesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var FormProductos = new FormProductos();
+            FormProductos.MdiParent = this;
+            FormProductos.Show();
+        }
+
+        private void loginToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            login();
+
+        }
+
+        private void login()
+        {
+            var FormLogin = new FormLogin();
+            FormLogin.ShowDialog();
+        }
+
+        private void clientesToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            var FormClientes = new FormClientes();
+            FormClientes.MdiParent = this;
+            FormClientes.Show();
+        }
+
+        private void ventasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var FormVentas = new FormVentas();
+            FormVentas.MdiParent = this;
+            FormVentas.Show();
+        }
+
+        private void FormMenu_Load(object sender, EventArgs e)
+        {
+            login();
+        }
+
+        private void reporteDeVentasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var FormReporteVentas = new FormReporteVentas();
+            FormReporteVentas.MdiParent = this;
+            FormReporteVentas.Show();
+        }
+
+        private void reporteDeClientesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var FormReporteCliente = new FormReporteCliente();
+            FormReporteCliente.MdiParent = this;
+            FormReporteCliente.Show();
+        }
+
+        private void reporteDeGarantiaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var FormReporteGarantia = new FormReporteGarantia();
+            FormReporteGarantia.MdiParent = this;
+            FormReporteGarantia.Show();
+        }
+    }
+}
---
+++ Win.Ventas/Login.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.Ventas
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Usuario;
+            string Contrasena;
+
+            Usuario = textBox1.Text;
+            Contrasena = textBox2.Text;
+
+            if (Usuario == "Tvarela" && Contrasena == "2324")
+             
+            {
+                this.Close();
+            }
+            else
+            { 
+                if (Usuario == "YSalgado" && Contrasena == "1516")
+                {
+                    this.Close();
+                }
+                else 
+                    MessageBox.Show("Usuario o Contraseña Incorrecta");
+            }
+    }
+    }
+}
+
+
